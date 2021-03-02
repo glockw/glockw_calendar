e54cca03cde383c3cd43d410fe13a49b88adc8eb
Gabriel Guzman <gguzmanlockward@gmail.com>
2021-03-02 17:58:15 -0400

    addding month header

--- components/Month.js
+++ components/Month.js
@@ -1,12 +1,13 @@
 import { useSelector } from "react-redux";
 import DaysOfTheWeek from "../components/widgets/DaysOfTheWeek";
 import Week from "./Week";
-
+import MonthHeader from "./widgets/MonthHeader";
 export default function Month() {
   const { month } = useSelector((state) => state);
 
   return (
     <div className="mx-auto">
+      <MonthHeader />
       <DaysOfTheWeek />
       {month.map((week, index) => (
         <Week key={`Week__${index}`} days={week} />
