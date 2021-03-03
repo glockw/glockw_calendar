5e3c729705463e0bdb3019e1588bd4a0e2fd59cc
Gabriel Guzman <gguzmanlockward@gmail.com>
2021-03-02 22:02:33 -0400

    cleaning the store

--- store.js
+++ store.js
@@ -3,10 +3,10 @@ import { applyMiddleware, createStore } from "redux";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import thunk from "redux-thunk";
-import { initialState, reducer } from "./reducer/reducer";
+import { reducer } from "./reducer/reducer";
 let store;
 
-function makeStore(state = initialState) {
+function makeStore() {
   const isServer = typeof window === "undefined";
 
   if (isServer) {
@@ -19,36 +19,23 @@ function makeStore(state = initialState) {
   };
   const persistedReducer = persistReducer(persistConfig, reducer);
   const store = makeConfiguredStore(persistedReducer);
-  store.__persistor = persistStore(store); // Nasty hack
+  store.__persistor = persistStore(store);
   return store;
 }
 
 const makeConfiguredStore = (reducer) =>
   createStore(reducer, undefined, applyMiddleware(thunk));
 
-export const initializeStore = (preloadedState) => {
-  let _store = store ?? makeStore(preloadedState);
+export const initializeStore = () => {
+  let _store = store ?? makeStore();
 
-  // After navigating to a page with an initial Redux state, merge that state
-  // with the current state in the store, and create a new store
-  if (preloadedState && store) {
-    _store = makeStore({
-      ...store.getState(),
-      ...preloadedState,
-    });
-    // Reset the current store
-    store = undefined;
-  }
-
-  // For SSG and SSR always create a new store
   if (typeof window === "undefined") return _store;
-  // Create the store once in the client
   if (!store) store = _store;
 
   return _store;
 };
 
 export function useStore(initialState) {
-  const store = useMemo(() => initializeStore(initialState), [initialState]);
+  const store = useMemo(() => initializeStore());
   return store;
 }
